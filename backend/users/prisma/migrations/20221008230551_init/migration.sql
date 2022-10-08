2545800bfeaed74bba22ab87b49e88c2fbe4e99c
Brayden Wong <braydenlwong@gmail.com>
2022-10-08 17:06:09 -0600

    updated user schema

--- backend/users/prisma/migrations/20221008230551_init/migration.sql
+++ backend/users/prisma/migrations/20221008230551_init/migration.sql
@@ -3,11 +3,12 @@ CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "first_name" VARCHAR(20) NOT NULL,
-    "last_name" VARCHAR(16) NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "email" VARCHAR(30) NOT NULL,
-    "phone_number" VARCHAR(10) NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone_number" TEXT NOT NULL,
+    "refresh_token" TEXT,
     "verified" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
@@ -18,3 +19,6 @@ CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_phone_number_key" ON "User"("phone_number");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_refresh_token_key" ON "User"("refresh_token");
