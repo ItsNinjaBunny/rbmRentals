ad7db0a76be727430f82a8bd4ca00afa4e862230
Brayden Wong <braydenlwong@gmail.com>
2022-10-08 22:21:59 -0600

    restructured file system

--- frontend/src/pages/RentalManagement.tsx
+++ frontend/src/pages/RentalManagement.tsx
@@ -1,8 +1,8 @@
-import { About } from "../components/Home/About"
-import { AllInOne } from "../components/Home/AllInOne"
-import { Hero } from "../components/Home/Hero"
-import { Pricing } from "../components/Home/Pricing"
-import { Support } from "../components/Home/Support"
+import { About } from "../components/RentalManagement/About"
+import { AllInOne } from "../components/RentalManagement/AllInOne"
+import { Hero } from "../components/RentalManagement/Hero"
+import { Pricing } from "../components/RentalManagement/Pricing"
+import { Support } from "../components/RentalManagement/Support"
 import { Footer } from "../components/shared/Footer/Footer"
 import { Navbar } from "../components/shared/Navbar/Navbar"
 
